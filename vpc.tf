903ad616a1e8e8b1bfad455f7437397e157da634
riyazul1 <riyazul.hasan@gmail.com>
2021-05-20 17:37:57 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "network" {
   source  = "app.terraform.io/Riyazul-training/network/google"
   version = "2.5.0"
   # insert required variables here
-  network_name = riyazul-network
+  network_name = "riyazul-network"
   project_id = var.project
   subnets = [
   {
